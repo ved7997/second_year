717cfb0d02aa37240f67abdd6c84477a2c9356be
ved7997 <ved7997@gmail.com>
2022-05-08 10:07:34 +0300

    Initial commit

---
+++ src/main/java/com/example/second_year/SecondYearApplication.java
@@ -0,0 +1,13 @@
+package com.example.second_year;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondYearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondYearApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/second_year/SecondYearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.second_year;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondYearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
